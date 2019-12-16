dbcab228553e4a5051b907027514b7934e549221
Nathanael Swander <swander926@yahoo.com>
2019-12-15 23:17:52 -0500

    initial cap push

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,49 @@ import Page from './pages/Page'
 import Page2 from './pages/Page2'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
+//why cant i import the photos here???
 
 const App = () => {
   return (
     <Router>
       <header>
-        <h1>Welcome to my SPA</h1>
         <nav>
-          <ul>
+          <a href="/">
+            <img src={'./images/samlogowhite.jpg'} className="headerLogo" />
+          </a>
+          <ul className="topNav">
             <li>
-              <Link to="/">Go Home</Link>
+              <Link to="/">HOME</Link>
             </li>
             <li>
-              <Link to="/1">Page 1</Link>
+              <Link to="/Services">SERVICES</Link>
             </li>
             <li>
-              <Link to="/2">Page 2</Link>
+              <Link to="/Cars">VEHICLES</Link>
+            </li>
+            <li>
+              <Link to="/Specials">SPECIALS</Link>
+            </li>
+            <li>
+              <Link to="/Appointments">APPOINTMENTS</Link>
+            </li>
+            <li>
+              <Link to="/About Us">ABOUT US</Link>
+            </li>
+            <li>
+              <Link to="/Reviews">REVIEWS</Link>
+            </li>
+            <li>
+              <Link to="/Directions">DIRECTIONS</Link>
             </li>
           </ul>
         </nav>
       </header>
       <Switch>
-        <Route exact path="/" component={HomePage}></Route>
+        {/* <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/1" component={Page}></Route>
         <Route exact path="/2" component={Page2}></Route>
-        <Route path="*" component={NotFound}></Route>
+        <Route path="*" component={NotFound}></Route> */}
       </Switch>
     </Router>
   )
