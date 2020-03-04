881754944ce41f2fc6c05cb156b1d5b0dcdd45d0
Nathanael Swander <swander926@yahoo.com>
2020-03-03 19:33:05 -0500

    Add Loading Screen for Reviews

--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -7,7 +7,8 @@ import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
 
 
 const ReviewsPage = () => {
-  const [reviews, setReviews] = useState([])
+  const [reviews, setReviews] = useState(null)
+  const [loaded, setLoaded] = useState(false)
 
   const getReviews = async () => {
     const resp = await Axios.get('https://samonlineback.herokuapp.com/api/Reviews')
@@ -17,45 +18,111 @@ const ReviewsPage = () => {
 
   useEffect(() => {
     getReviews()
+    // setLoaded(false)
   }, [])
+
+  useEffect(() => {
+    if (reviews !== null) {
+      setLoaded(true)
+    }
+  }, [reviews])
+
 
   return (
     <>
-    <Page withPadding={false}>
-      <div className="manualPaddingDiv"><BreadcrumbsComponent nextOne={'Reviews'} nextOneUrl={'/reviews'} />
-      <div className="reviewTable">
-        {reviews.map(reviews => {
-          return (
-            <div className="reviewBox">
-              <ul className="reviewList">
-                <div className="reviewInfo">
-                  <li>{reviews.name}</li>
-                  <li className="greyText">{reviews.date}</li>
-                  <li className="starsImg">
-                    <img
-                      alt="5 Star Review Image"
-                      height="30px"
-                      width="150px"
-                      src="./images/reviews.png"
-                    />
-                  </li>
-                </div>
-                <div className="reviewText">
-                  <li>{reviews.reviewText}</li>
-                </div>
-              </ul>
-            </div>
-          )
-        })}
-      </div></div>
-      <div className="reviewSubmitButtonContainer">
-        <div className="reviewSubmitButtonInnerDiv">
-        <a href="/SubmitReviewPage" className="reviewSubmitButton" type="Submit">Leave A Review!</a></div></div>
-      
-      
+    { !loaded ? (
+    <>
+    <Page>
+    <h1>Loading...</h1>
     </Page>
     </>
+) : (
+    <>
+          <Page withPadding={false}>
+            <div className="manualPaddingDiv"><BreadcrumbsComponent nextOne={'Reviews'} nextOneUrl={'/reviews'} />
+            <div className="reviewTable">
+              {reviews.map(reviews => {
+                return (
+                  <div className="reviewBox">
+                    <ul className="reviewList">
+                      <div className="reviewInfo">
+                        <li>{reviews.name}</li>
+                        <li className="greyText">{reviews.date}</li>
+                        <li className="starsImg">
+                          <img
+                            alt="5 Star Review Image"
+                            height="30px"
+                            width="150px"
+                            src="./images/reviews.png"
+                          />
+                        </li>
+                      </div>
+                      <div className="reviewText">
+                        <li>{reviews.reviewText}</li>
+                      </div>
+                    </ul>
+                  </div>
+                )
+              })}
+            </div></div>
+            <div className="reviewSubmitButtonContainer">
+              <div className="reviewSubmitButtonInnerDiv">
+              <a href="/SubmitReviewPage" className="reviewSubmitButton" type="Submit">Leave A Review!</a></div></div>
+               
+          </Page>
+              </>
   )
+    }
+    </>
+  )
+
+// if (!loaded) {
+//   return (
+//     <>
+//     <Page>
+//     <h1>Loading...</h1>
+//     </Page>
+//     </>
+//   )
+//       } else {
+        // return (
+        //   <>
+        //   <Page withPadding={false}>
+        //     <div className="manualPaddingDiv"><BreadcrumbsComponent nextOne={'Reviews'} nextOneUrl={'/reviews'} />
+        //     <div className="reviewTable">
+        //       {reviews.map(reviews => {
+        //         return (
+        //           <div className="reviewBox">
+        //             <ul className="reviewList">
+        //               <div className="reviewInfo">
+        //                 <li>{reviews.name}</li>
+        //                 <li className="greyText">{reviews.date}</li>
+        //                 <li className="starsImg">
+        //                   <img
+        //                     alt="5 Star Review Image"
+        //                     height="30px"
+        //                     width="150px"
+        //                     src="./images/reviews.png"
+        //                   />
+        //                 </li>
+        //               </div>
+        //               <div className="reviewText">
+        //                 <li>{reviews.reviewText}</li>
+        //               </div>
+        //             </ul>
+        //           </div>
+        //         )
+        //       })}
+        //     </div></div>
+        //     <div className="reviewSubmitButtonContainer">
+        //       <div className="reviewSubmitButtonInnerDiv">
+        //       <a href="/SubmitReviewPage" className="reviewSubmitButton" type="Submit">Leave A Review!</a></div></div>
+            
+            
+        //   </Page>
+        //   </>
+        // )
+      // }
 }
 
 export default ReviewsPage
